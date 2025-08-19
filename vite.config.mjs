c6d9238100b0be5be1302a91dfebe5a679862e7d
anandmoorthy <anandmoorthyj@gmail.com>
2025-08-19 15:11:54 +0530

    vite config change to support mycaldo.xyz

--- vite.config.mjs
+++ vite.config.mjs
@@ -6,8 +6,10 @@ const explicitBase = process.env.BASE_PATH; // e.g. "/caldo/"
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
 const isCI = process.env.GITHUB_ACTIONS === 'true';
 
+// export default defineConfig({
+//   base: explicitBase ?? (isCI && repo ? `/${repo}/` : '/'),
+// });
+
 export default defineConfig({
-  base: explicitBase ?? (isCI && repo ? `/${repo}/` : '/'),
+  base: '/',
 });
-
-
